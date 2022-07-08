9315644a9f5a4f89c92160e559f5211ba98cd544
quytram007 <quytram007@gmail.com>
2022-07-08 17:24:37 +0700

    Tao Khung Giao Dien

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-master">
+      <nav className='navbar app'> App bar </nav>
+      <nav className='navbar board'>Board bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='http://cdn.shopify.com/s/files/1/0600/5650/2485/products/38126-0w470h470_Red_Dragon_Fruit.jpg?v=1646225932' />
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
